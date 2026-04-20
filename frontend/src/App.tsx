a7772494fe8d83e0876fd7fa1d37233ba744db45
himarnoel <olaimarnoel@gmail.com>
2026-04-20 22:01:22 +0100

    refactor: restructure App component to include QueryClientProvider and TooltipProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,34 @@
-import './App.css'
+import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { retry: 1, refetchOnWindowFocus: false, staleTime: 30_000 },
+    },
+  });
   return (
     <>
-     <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Sonner richColors position="top-right" />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
