00f57b664dddfb75e61fe8961225e07e1f5a128a
himarnoel <olaimarnoel@gmail.com>
2026-04-20 21:28:48 +0100

    fix: reorder import statements for consistency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
